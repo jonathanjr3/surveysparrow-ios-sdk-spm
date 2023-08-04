677632dbb41023f232de03db40f24d40a81c65b8
jonathanjr3 <jonathan.rajya@decathlon.com>
2023-08-04 18:57:52 +0530

    Changed package name

--- Package.swift
+++ Package.swift
@@ -4,25 +4,25 @@
 import PackageDescription
 
 let package = Package(
-    name: "surveysparrow-ios-sdk-spm",
+    name: "SurveySparrow",
     platforms: [
         .iOS(.v11), .macOS(.v10_13)
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "surveysparrow-ios-sdk-spm",
-            targets: ["surveysparrow-ios-sdk-spm"]),
+            name: "SurveySparrow",
+            targets: ["SurveySparrow"]),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "surveysparrow-ios-sdk-spm",
+            name: "SurveySparrow",
             path: "SurveySparrow/SurveySparrowSdk"),
         .testTarget(
-            name: "surveysparrow-ios-sdk-spmTests",
-            dependencies: ["surveysparrow-ios-sdk-spm"],
+            name: "SurveySparrowTests",
+            dependencies: ["SurveySparrow"],
             path: "SurveySparrow/SurveySparrowSdkTests"),
     ]
 )
